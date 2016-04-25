f234e03e07e651110c133e51f66636c028ced527
David Virtser <poison77@gmail.com>
2016-04-25 17:38:31 +0300

    Added better version support

--- tasks/autoupdate.js
+++ tasks/autoupdate.js
@@ -13,28 +13,70 @@ module.exports = function (grunt) {
   grunt.registerTask('autoupdate', 'Auto updates the node_modules if the package.json version changed', function () {
 
     var options = this.options({
-      pkgFile : '.pkg'
+      pkgFile : '.pkg',
+      npmCheck: true,
+      bowerCheck: true,
+      warnOnly: false
     });
 
-    var previousPkgVersion = 'N/A',
-      pkgVersion = grunt.file.readJSON('package.json').version;
+    var pkgFileContent = {},
+        prevPkgFileContent = {},
+        firstTimeRun = false,
+        warnArray = [];
 
-    if (grunt.file.exists(options.pkgFile)) {
-      previousPkgVersion = grunt.file.read(options.pkgFile);
+    if(options.npmCheck) {
+      var npmVersion = grunt.file.readJSON('package.json').version;
+      pkgFileContent.npmVersion = npmVersion;
     }
 
-    grunt.file.write(options.pkgFile, pkgVersion);
+    if(options.bowerCheck) {
+      var bowerVersion = grunt.file.readJSON('bower.json').version;
+      pkgFileContent.bowerVersion = bowerVersion;
+    }
 
-    if( previousPkgVersion !== pkgVersion ){
-      grunt.log.subhead('New package.json version detected (from '+previousPkgVersion+' to '+pkgVersion+')');
-      
-      grunt.log.writeln('Running npm install...');
-      require('child_process').execSync('npm install', {stdio:[0,1,2]});
+    if (grunt.file.exists(options.pkgFile)) {
+      prevPkgFileContent = grunt.file.readJSON(options.pkgFile);
+    } else {
+      firstTimeRun = true;
+      grunt.log.writeln('Creating configuration file for the first time: ' + options.pkgFile);
+    }
 
-      grunt.log.writeln('Running bower install...');
-      require('child_process').execSync('bower install', {stdio:[0,1,2]});
+    grunt.file.write(options.pkgFile, JSON.stringify(pkgFileContent, null, "\t"));
 
-      grunt.log.writeln('... done updating module dependencies!');
+    var runShellCommand = function(cmd) {
+      grunt.log.writeln('Running: ' + cmd);
+      require('child_process').execSync(cmd, {stdio:[0,1,2]});
+
+      grunt.log.writeln('Finished execution of: ' + cmd);
+    };
+
+    if(!firstTimeRun && JSON.stringify(prevPkgFileContent) !== JSON.stringify(pkgFileContent)){
+      if (options.npmCheck) {
+        if (prevPkgFileContent.npmVersion !== pkgFileContent.npmVersion) {
+            grunt.log.subhead('New package.json version detected (from ' + prevPkgFileContent.npmVersion + ' to ' + pkgFileContent.npmVersion + ').');
+
+          if (options.warnOnly) {
+            warnArray.push('New changes detected in package.json file. Run "npm install" to update.\n');
+          } else {
+            runShellCommand('npm install');
+          }
+        }
+      }
+
+      if (options.bowerCheck) {
+        if (prevPkgFileContent.bowerVersion !== pkgFileContent.bowerVersion) {
+            grunt.log.subhead('New bower.json version detected (from ' + prevPkgFileContent.bowerVersion + ' to ' + pkgFileContent.bowerVersion + ').');
+
+          if (options.warnOnly) {
+            warnArray.push('New changes detected in bower.json file. Run "bower install" to update.\n');
+          } else {
+            runShellCommand('bower install');
+          }
+        }
+      }
+
+      if (warnArray)
+        grunt.warn(warnArray.toString());
 
       var args = process.argv.splice(2, process.argv.length);
       args.unshift('grunt');
@@ -42,10 +84,11 @@ module.exports = function (grunt) {
 
       grunt.log.ok('Please run \''+ command +'\' again');
       grunt.util.exit(0);
-    }else{
-      grunt.verbose.writeln('package.json was not updated');
+    } else {
+      if (options.npmCheck)
+        grunt.verbose.writeln('package.json was not updated');
+      if (options.bowerCheck)
+        grunt.verbose.writeln('bower.json was not updated');
     }
-
   });
-
 };
